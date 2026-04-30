fd79117dfb7b8d5f03eded1e76f93f69016c4796
Seng-Mouyheang <sengmouyheang2207@gmail.com>
2026-05-01 00:07:01 +0700

    feat: asset history page #28

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/', clientRouter)
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-	// console.log(`Server running at http://localhost:${port}`)
+	console.log(`Server running at http://localhost:${port}`)
 })
